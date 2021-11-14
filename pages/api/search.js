b7a0417142a3ff4b19eb930e17cb37181eb8ff4c
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2021-11-14 19:48:53 +0300

    added cache

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,39 +1,39 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (req, res) => {
-  let posts
+  let posts;
 
-  if (process.env.NODE_ENV === 'production') {
-    // @todo - fetch from cache
+  if (process.env.NODE_ENV === "production") {
+    posts = require("../../cache/data").posts;
   } else {
-    const files = fs.readdirSync(path.join('posts'))
+    const files = fs.readdirSync(path.join("posts"));
 
     posts = files.map((filename) => {
-      const slug = filename.replace('.md', '')
+      const slug = filename.replace(".md", "");
 
       const markdownWithMeta = fs.readFileSync(
-        path.join('posts', filename),
-        'utf-8'
-      )
+        path.join("posts", filename),
+        "utf-8",
+      );
 
-      const { data: frontmatter } = matter(markdownWithMeta)
+      const { data: frontmatter } = matter(markdownWithMeta);
 
       return {
         slug,
         frontmatter,
-      }
-    })
+      };
+    });
   }
 
   const results = posts.filter(
     ({ frontmatter: { title, excerpt, category } }) =>
       title.toLowerCase().indexOf(req.query.q) != -1 ||
       excerpt.toLowerCase().indexOf(req.query.q) != -1 ||
-      category.toLowerCase().indexOf(req.query.q) != -1
-  )
+      category.toLowerCase().indexOf(req.query.q) != -1,
+  );
 
-  res.status(200).json(JSON.stringify({ results }))
-}
+  res.status(200).json(JSON.stringify({ results }));
+};
