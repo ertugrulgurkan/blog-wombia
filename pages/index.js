a554146985c03eb3a01f5fee050afbb8421b07eb
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2021-11-14 14:21:52 +0300

    display post sort post by date post component added

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import Layout from "../components/layout";
 import fs from "fs";
 import path from "path";
+import Link from "next/link";
 import matter from "gray-matter";
+import Post from "../components/Post";
+import { sortByDate } from "../utils";
 
 export default function HomePage({ posts }) {
-  console.log(posts);
   return (
     <Layout>
-      <h1>Hello World!</h1>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Latest Posts</h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+
+      <Link href="/blog">
+        <a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">
+          All Posts
+        </a>
+      </Link>
     </Layout>
   );
 }
@@ -22,9 +36,7 @@ export async function getStaticProps() {
       path.join("posts", filename),
       "utf-8",
     );
-
     const { data: frontmatter } = matter(markdownWithMeta);
-
     return {
       slug,
       frontmatter,
@@ -33,7 +45,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts,
+      posts: posts.sort(sortByDate).slice(0, 6),
     },
   };
 }
