34c1906943ed65dd8ec5856bbe102756f7adbd0e
Ertuğrul Gürkan <ertugrulgurkan16@gmail.com>
2021-11-14 13:54:41 +0300

    Fetch markdown posts and set props added gray-matter dep

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,39 @@
 import Layout from "../components/layout";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
-export default function HomePage() {
+export default function HomePage({ posts }) {
+  console.log(posts);
   return (
     <Layout>
       <h1>Hello World!</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8",
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
